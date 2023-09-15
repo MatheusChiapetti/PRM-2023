8971bb3e0a4659cea143eb03452c7d50d000d619
Matheus Henrique Michels Chiapetti <m.henrique2002@gmail.com>
2023-09-15 13:41:23 -0300

    Aula 05/09/2023 (15/09/2023)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProfileService } from './services/profile.service';
 import { ProfileController } from './controllers/profile.controller';
+import { ProfileService } from './services/profile.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'prm_2023',
+      entities: [],
+      synchronize: true,
+    })
+	],
   controllers: [AppController, ProfileController],
   providers: [AppService, ProfileService],
 })
-export class AppModule {}
+export class AppModule { }
